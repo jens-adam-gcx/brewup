a03b8105a9028c440aa13593327393dd5b9551eb
Jens Adam <jens.adam@grandcentrix.net>
2018-10-10 18:35:11 +0200

    add prune, revert to old '&&' usage

--- brewup.sh
+++ brewup.sh
@@ -1,6 +1,6 @@
-#!/bin/bash -eu
-
+#!/bin/bash -u
 echo "===> upgrading all the 🍺"
+
 echo "[+] missing"
 brew missing
 
@@ -8,20 +8,20 @@ echo "[+] doctor"
 brew doctor
 
 echo "[+] update"
-brew update
+brew update &&
 
-echo "[+] cleanup"
-brew cleanup -s
+echo "[+] cleanup, prune"
+brew cleanup -s &&
+brew prune &&
 
 echo "[+] upgrade"
-brew upgrade --cleanup
+brew upgrade --cleanup &&
 
 echo "[+] clean cache"
-rm -rf $(brew --cache)
+rm -rf $(brew --cache) &&
 
 echo "[+] cask upgrade/reinstall"
-brew cask upgrade
-echo
+brew cask upgrade &&
 for CASK in $(brew cask ls); do
   if [ $(brew cask info ${CASK} 2>/dev/null | head -3 | grep -v '^http' | sed -e "s/ (auto_updates)$//" -e "s/^.*\/\(.*\) (.*$/${CASK}: \1/" | sort -u | wc -l) != "1" ]; then
     brew cask reinstall ${CASK}
