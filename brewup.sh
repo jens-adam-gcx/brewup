ce56e32eb250d622fd7ae45d9d04f8a56b8045e2
Jens Adam <jens.adam@grandcentrix.net>
2019-01-10 18:53:02 +0100

    :fireworks:
    * care less about cleanup, incorporate updates from
      https://gist.github.gcxi.de/aklute/a6d614581b1f50db2e275afc547c98e7
    * ignore 'cask upgrade', just rely on home-grown parsing and 'reinstall' again

--- brewup.sh
+++ brewup.sh
@@ -1,27 +1,15 @@
 #!/bin/bash -u
+export HOMEBREW_INSTALL_CLEANUP=1
 echo "===> upgrading all the 🍺"
-
 echo "[+] missing"
 brew missing
-
 echo "[+] doctor"
 brew doctor
-
 echo "[+] update"
 brew update &&
-
-echo "[+] cleanup, prune"
-brew cleanup -s &&
-brew prune &&
-
 echo "[+] upgrade"
-brew upgrade --cleanup &&
-
-echo "[+] clean cache"
-rm -rf $(brew --cache) &&
-
-echo "[+] cask upgrade/reinstall"
-brew cask upgrade &&
+brew upgrade &&
+echo "[+] cask upgrade"
 for CASK in $(brew cask ls); do
   if [ $(brew cask info ${CASK} 2>/dev/null | head -3 | grep -v '^http' | sed -e "s/ (auto_updates)$//" -e "s/^.*\/\(.*\) (.*$/${CASK}: \1/" | sort -u | wc -l) != "1" ]; then
     brew cask reinstall ${CASK}
